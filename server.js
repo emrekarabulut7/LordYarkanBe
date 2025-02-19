8101ab4b12b26d627ef89a911ed8f49c4e62dca8
Emre Karabulut <emre_karabulut@msn.com>
2025-02-19 22:30:21 +0300

    google_last76

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 
 // CORS ayarları
 app.use(cors({
-  origin: ['https://www.lordyarkan.com', 'https://lordyarkan.com'],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
 }));
